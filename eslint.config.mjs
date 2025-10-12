bdae6aef058aab4ce3961b1904832441df921b57
sunwane <auxuanhoa@gmail.com>
2025-10-13 01:04:40 +0700

    no more eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -17,8 +18,6 @@ const eslintConfig = [
     // Thêm hoặc ghi đè các quy tắc ESLint tại đây nếu cần
     "react/react-in-jsx-scope": "off", // Next.js tự động import React
     "react/jsx-uses-react": "off", // Next.js tự động import React
-    "no-unused-vars": "warn", // Cảnh báo biến không sử dụng
-    "@typescript-eslint/no-unused-vars": "warn", // Cảnh báo biến TypeScript không sử dụng
     },
   }),
 ];
