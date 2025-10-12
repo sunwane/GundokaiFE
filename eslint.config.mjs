db406b4e5363ea508a0c747fefbe1afd7c1e1f83
sunwane <auxuanhoa@gmail.com>
2025-10-13 01:49:42 +0700

    try to disable eslint again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,11 +12,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
     extends: ("next/core-web-vitals", "next/typescript"),
     rules: {
-    // Thêm hoặc ghi đè các quy tắc ESLint tại đây nếu cần
-    "react/react-in-jsx-scope": "off", // Next.js tự động import React
-    "react/jsx-uses-react": "off", // Next.js tự động import React
     },
   }),
 ];
