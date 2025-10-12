b43fde9c63f03a7b50bd0c0b46932ef2a299b179
sunwane <auxuanhoa@gmail.com>
2025-10-13 01:05:07 +0700

    no eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import { RuleTester } from "eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
