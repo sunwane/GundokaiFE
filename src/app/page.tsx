b6e0a1895be78ff0734dfb626df89cd08cab1e09
sunwane <auxuanhoa@gmail.com>
2025-10-13 14:49:14 +0700

    test suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 'use client';
-export const dynamic = "force-dynamic";
-
 import PageHeader from "@/component/layout/header/PageHeader";
 import BannerList from "@/component/features/home/BannerList";
 import ProductCategoryIntro from "@/component/features/home/ProductCategoryIntro";
@@ -9,9 +7,8 @@ import ProductShowcase from "@/component/features/home/ProductShowcase";
 import Footer from "@/component/layout/footer/Footer";
 import { useResponsive } from "@/hooks/useResponsive";
 import { banners, categories, gundamModels } from "@/data/homeData";
-import React, { Suspense } from "react";
 
-function HomeContent() {
+export default function Home() {
   const { isMobile } = useResponsive({ mobile: 400 });
 
   return (
@@ -23,24 +20,6 @@ function HomeContent() {
       <ProductShowcase isMobile={isMobile} />
       <Footer />
     </div>
-  );
-}
-
-export default function Home() {
-  return (
-    <Suspense fallback={
-      <div style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        backgroundColor: "#fff"
-      }}>
-        <div>Đang tải...</div>
-      </div>
-    }>
-      <HomeContent />
-    </Suspense>
   );
 }
 
